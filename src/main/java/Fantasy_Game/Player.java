4c49b145b6e983b505f3ea400c6aade5182007e1
Paul Stewart <stewartpaul22@yahoo.co.uk>
2018-03-08 13:40:12 +0000

    Add Player abstract class

--- src/main/java/Fantasy_Game/Player.java
+++ src/main/java/Fantasy_Game/Player.java
@@ -1,4 +1,19 @@
 package Fantasy_Game;
 
 public class Player {
+    private String name;
+    private int health;
+
+    public Player(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
 }
